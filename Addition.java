fb0054fa06513ed563049f8c1fd577242ec2bc03
GayathriSugumar <90204269+GayathriSugumar@users.noreply.github.com>
2021-09-11 22:21:32 +0530

    Addition program initial commit

---
+++ Addition.java
@@ -0,0 +1,18 @@
+class Addition {
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 20;
+		double c = 30.5;
+		double d = 40.5;
+		int e;
+		double f;
+		double g;
+		e = a + b;
+		f = c + d;
+		g = a + c;
+		System.out.println("The addition of a and b is " + e);
+		System.out.println("The addition of c and d is " + f);
+		System.out.println("The addition of a and c is " + g);
+
+	}
+}
